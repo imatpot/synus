967a3674ec000950df5cb236ac73c6aea19203fa
Mladen Brankovic <mladenbrankovic@protonmail.ch>
2019-04-10 22:17:11 +0200

    Added laugh command

---
+++ commands/memes/laugh.js
@@ -0,0 +1,14 @@
+const echo = require('../general/echo.js').execute;
+const print = require('../general/print.js').execute;
+
+module.exports.properties = {
+    name: 'laugh',
+    aliases: ['xd','haha','hah'],
+    description: 'Make Synus laugh. Removed request to improve comedic effect.',
+    usage: 'synus laugh',
+}
+
+module.exports.execute = (args, message, bot) => {
+    message.delete(1000);
+    echo(bot.emojis.get('519958500164501515').toString(), message);
+}
